35d6397cf3d3ef46c39cd1fe5b987fa139f7a7cc
Mahendra-1996 <mahendra_gupta@webixun.com>
2021-09-13 12:24:44 +0530

    push to user

--- database/migrations/2021_08_18_092528_create_users_table.php
+++ database/migrations/2021_08_18_092528_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('salutation');
             $table->string('name');
             $table->string('pic')->nullable();
             $table->string('email')->nullable();
